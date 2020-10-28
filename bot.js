0105354a4e96e1aef836060936acc89bbc9d0276
Millard Arnold V <clear.the.spam@gmail.com>
2020-10-27 19:13:44 -0800

    Update bot.js
    More useful Messages

--- bot.js
+++ bot.js
@@ -15,18 +15,19 @@ client.on('message', message => {
   const command = args.shift().toLowerCase();
   const classes = ['math-251','es-101','chem-105','math-252','es-201','chem-106','math-253','phys-211','es-209','me-321','math-302','phys-212','es-210','es-346','es-301','es-331','me-302','es-341','es-307','me-308','me-334','me-313','me-408','me-486','me-441','esm-450','me-487','me-403','me-415','ge-261','ce-112','ce-334','ce-341','ce-344','ce-432','ce-331','ce-302','esm-422','ce-470','ce-437','ce-438','ce-471'];
   //add class roles
-  if (command === 'join') {
-    if (!classes.includes(args[0])){
+  if (command === 'join')
+  {
+    if (!classes.includes(args[0]))
+    {
       message.channel.send(args[0] + " is not a class.");
       return;
     }
     const role = message.guild.roles.cache.find(role=>role.name===args[0]);
     const member = message.member;
     member.roles.add(role).catch(console.error);
-	  message.channel.send('done');
+	  message.channel.send('You have joined ' + args[0]);
 	}
-  //remove class roles
-  if (command === 'leave')
+  else if (command === 'leave')
   {
     if(!classes.includes(args[0]))
     {
@@ -36,7 +37,17 @@ client.on('message', message => {
     const role = message.guild.roles.cache.find(role=>role.name===args[0]);
     const member = message.member;
     member.roles.remove(role).catch(console.error);
-    message.channel.send('done');
+    message.channel.send('You have left ' + args[0]);
+  }
+  else if (command === 'help')
+  {
+    message.channel.send('!join CLASS_CODE')
+    message.channel.send('!leave CLASS_CODE')
+    message.channel.send('!help')
+  }
+  else
+  {
+    message.channel.send(command + 'is not an acceptable command')
   }
 });
 
