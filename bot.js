3a8012a8b677e1093b5614a90703f3bf1df56ece
lmvanclifford <73456905+lmvanclifford@users.noreply.github.com>
2020-10-30 22:42:22 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ client.on('message', message => {
   //Split message into command and arguments
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
-  const classes = ['math-251','me-458','me-450','es-101','chem-105','math-252','es-201','chem-106','math-253','phys-211','es-209','me-321','math-302','phys-212','es-210','es-346','es-301','es-331','me-302','es-341','es-307','me-308','me-334','me-313','me-408','me-486','me-441','esm-450','me-487','me-403','me-415','ge-261','ce-112','ce-334','ce-341','ce-344','ce-432','ce-331','ce-302','esm-422','ce-470','ce-437','ce-438','ce-471'];
+  const classes = ['me-254','me-402','me-405','me-406','me-409','me-414','me-416','me-431','me-440','me-443','me-451','me-452','me-453','me-464','ce-326','ce-405','ce-422','ce-424','ce-433','ce-434','ce-435','ce-442','ce-443','ce-445','ce-451','ce-463','math-251','me-458','me-450','es-101','chem-105','math-252','es-201','chem-106','math-253','phys-211','es-209','me-321','math-302','phys-212','es-210','es-346','es-301','es-331','me-302','es-341','es-307','me-308','me-334','me-313','me-408','me-486','me-441','esm-450','me-487','me-403','me-415','ge-261','ce-112','ce-334','ce-341','ce-344','ce-432','ce-331','ce-302','esm-422','ce-470','ce-437','ce-438','ce-471'];
   //add class roles
   if (command === 'join')
   {
